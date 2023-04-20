9962b419315210e2d3325677192413f453ad757b
ThereIsGrace <universe2879@gmail.com>
2023-04-20 21:00:50 +0900

    float형과 double형의 차이와 강제 형변환

---
+++ t20230419/Test03.java
@@ -0,0 +1,11 @@
+package t20230419;
+
+public class Test03 {
+// 8진수 12, 10진수 12, 16진수 12를 각각 10진수로 표시하는 프로그램을 작성하자.
+	public static void main(String[] args) {
+		
+		System.out.println("8진수 12는 10진수로 " + 012 + "입니다.");
+		System.out.println("10진수 12는 10진수로 " + 12 + "입니다.");
+		System.out.println("16진수 12는 10진수로 " + 0x12 + "입니다.");
+	}
+}
