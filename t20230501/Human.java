85a4a42185a84d3ab304d92598db6d896b46bfba
ThereIsGrace <universe2879@gmail.com>
2023-05-01 18:01:40 +0900

    객체 활용

---
+++ t20230501/Human.java
@@ -0,0 +1,34 @@
+package t20230501;
+
+public class Human {
+	String name;      // 이름
+	int height;       // 신장
+	int weight;       // 체중
+	
+	
+	public Human(String name, int height, int weight) {
+		super();
+		this.name = name;
+		this.height = height;
+		this.weight = weight;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+}
