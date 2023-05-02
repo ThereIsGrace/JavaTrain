850e5d44c125b2b5e20708e60ad7b19b30e9b9de
ThereIsGrace <universe2879@gmail.com>
2023-05-02 20:45:23 +0900

    클래스 내의 메서드 만들기

---
+++ t20230502/test01/Id.java
@@ -0,0 +1,16 @@
+package t20230502.test01;
+// 일련 번호 클래스(Ver.1)
+class Id {
+	static int counter = 0;      // 몇 번까지 식별 번호를 부여했는가
+	private int id;              // 식별 번호
+	
+	//--- 생성자 ---//
+	public Id() {
+		id = ++counter;         // 식별 번호
+	}
+	
+	//--- 식별 번호 가져오기 ---//
+	public int getId() {
+		return id;
+	}
+}
