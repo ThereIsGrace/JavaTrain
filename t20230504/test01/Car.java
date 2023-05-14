6be316baa232d730ef2b5eae079ea535c90e9ed1
ThereIsGrace <universe2879@gmail.com>
2023-05-14 23:42:48 +0900

    자동차 클래스 만들기

---
+++ t20230504/test01/Car.java
@@ -0,0 +1,13 @@
+package t20230504.test01;
+//자동차 클래스(Ver.2)
+public class Car {
+	private String name;            //이름
+	private int width;              //폭
+	private int height;             //높이
+	private int length;             //길이
+	private double x;               //현재 위치의 X좌표
+	private double y;               //현재 위치의 Y좌표
+	private double fuel;            //남은 연료
+	//private Day purchaseDay;        //구입일
+
+}
